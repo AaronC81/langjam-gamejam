e4267670b2841d83bae22457d6dbcc09420514f6
Aaron Christiansen <aaronc20000@gmail.com>
2025-12-15 20:02:20 +0000

    Delete default crate stuff

--- lang/src/lib.rs
+++ lang/src/lib.rs
@@ -6,21 +6,3 @@ pub use interpreter::*;
 
 mod parser;
 pub use parser::*;
-
-#[cfg(test)]
-mod test;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
