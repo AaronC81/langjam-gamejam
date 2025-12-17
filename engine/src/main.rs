b1c4f85c2a383356bf030b2297f6f672d59f74f0
Aaron Christiansen <aaronc20000@gmail.com>
2025-12-17 20:06:35 +0000

    Break out game into separate file

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -10,64 +10,7 @@ fn main() {
         .build();
     rl.set_target_fps(60);
 
-    let declarations = parse("
-        entity FpsTest {
-            var @ticks;
-
-            constructor {
-                @ticks = 0;
-            }
-
-            tick {
-                @ticks = @ticks + 1;
-                echo @ticks;
-            }
-        }
-        
-        entity Smile {
-            var @x, @y, @move_cooldown;
-
-            constructor {
-                @x = 2;
-                @y = 2;
-                @move_cooldown = 0;
-            }
-
-            tick {
-                if (@move_cooldown > 0) {
-                    @move_cooldown = @move_cooldown - 1;
-                }
-
-                if (Input.down_pressed()) {
-                    if (@move_cooldown == 0) {
-                        @y = @y + 1;
-                        @move_cooldown = 3;
-                    }
-                }
-
-                if (Input.up_pressed()) {
-                    if (@move_cooldown == 0) {
-                        @y = @y - 1;
-                        @move_cooldown = 3;
-                    }
-                }
-            }
-
-            draw {
-                return sprite {
-                    .#.#.
-                    .....
-                    #...#
-                    .###.
-                };
-            }
-        }
-
-        constructor {
-            spawn FpsTest;
-            spawn Smile;
-        }
-    ").unwrap();
+    let declarations = parse(include_str!("../../game/main.utl")).unwrap();
     let mut interpreter = Interpreter::with_declarations(&declarations).unwrap();
 
     interpreter.execute_init().unwrap();
