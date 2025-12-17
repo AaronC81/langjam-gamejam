75ec503f3980639b0984a3e82b01d563f2515c1f
Aaron Christiansen <aaronc20000@gmail.com>
2025-12-17 21:21:49 +0000

    Finally add syntax for functions

--- lang/src/parser/declaration.rs
+++ lang/src/parser/declaration.rs
@@ -1,4 +1,4 @@
-use nom::{IResult, Parser, branch::alt, bytes::complete::tag, combinator::map, multi::{many0, separated_list1}};
+use nom::{IResult, Parser, branch::alt, bytes::complete::tag, character::complete::char, combinator::map, multi::{many0, separated_list0, separated_list1}};
 
 use crate::{Declaration, Statement, parser::{declaration_body, identifier, instance_var_identifier, statement::statement, statement_body, ws0, ws1}};
 
@@ -14,6 +14,23 @@ fn instance_var_declaration(input: &str) -> IResult<&str, Declaration> {
     ).parse(input)
 }
 
+fn function_declaration(input: &str) -> IResult<&str, Declaration> {
+    map(
+        (
+            tag("func"),
+            ws1,
+            identifier,
+            ws0,
+            char('('),
+            separated_list0((ws0, char(','), ws0), identifier),
+            char(')'),
+            ws0,
+            statement_body,
+        ),
+        |(_, _, name, _, _, parameters, _, _, body)| Declaration::FunctionDeclaration { name, parameters, body }
+    ).parse(input)
+}
+
 pub fn declaration(input: &str) -> IResult<&str, Declaration> {
     alt((
         map((tag("entity"), ws1, identifier, ws0, declaration_body), |(_, _, name, _, body)| Declaration::EntityDeclaration { name, body }),
@@ -21,6 +38,6 @@ pub fn declaration(input: &str) -> IResult<&str, Declaration> {
         map((tag("tick"), ws0, statement_body), |(_, _, body)| Declaration::TickDeclaration { body }),
         map((tag("draw"), ws0, statement_body), |(_, _, body)| Declaration::DrawDeclaration { body }),
         instance_var_declaration,
-        // TODO: function
+        function_declaration,
     )).parse(input)
 }
